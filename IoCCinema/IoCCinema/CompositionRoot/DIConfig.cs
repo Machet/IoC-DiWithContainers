b792b76ad7891a9050eeec4362ba63800301a510
Mariusz Macheta <Mariusz.Macheta@gmail.com>
2015-11-21 18:20:28 +0100

    Registering types for LoginController

--- IoCCinema/IoCCinema/CompositionRoot/DIConfig.cs
+++ IoCCinema/IoCCinema/CompositionRoot/DIConfig.cs
@@ -1,6 +1,11 @@
-﻿using IoCCinema.DataAccess.Presentation;
+﻿using IoCCinema.Business.Authentication;
+using IoCCinema.Business.Commands;
+using IoCCinema.DataAccess;
+using IoCCinema.DataAccess.Business;
+using IoCCinema.DataAccess.Presentation;
 using IoCCinema.Presentation;
 using SimpleInjector;
+using SimpleInjector.Integration.Web;
 using SimpleInjector.Integration.Web.Mvc;
 using System.Web.Mvc;
 
@@ -11,8 +16,18 @@ namespace IoCCinema.CompositionRoot
         public static void Setup()
         {
             var container = new Container();
+            var perRequest = new WebRequestLifestyle();
 
-            container.Register<IMovieViewRepository, EfMovieViewRepository>();
+            container.Register<CinemaContext>(perRequest);
+            container.Register<ICurrentUserProvider, ContextUserProvider>(Lifestyle.Singleton);
+
+            container.Register<IMovieViewRepository, EfMovieViewRepository>(perRequest);
+            container.Register<ILoginViewRepository, EfLoginRepository>(perRequest);
+            container.Register<IAuthenticationRepository, EfAuthenticationRepository>(perRequest);
+
+            container.RegisterDecorator<ICommandHandler<LoginCommand>, TransactionalCommandHandler<LoginCommand>>();
+            container.Register<ICommandHandler<LoginCommand>, LoginCommandHandler>();
+
             DependencyResolver.SetResolver(new SimpleInjectorDependencyResolver(container));
         }
     }
