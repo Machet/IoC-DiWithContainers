2710347e6bd2cdb5a0ceb94628b81b795371e61b
Mariusz Macheta <Mariusz.Macheta@gmail.com>
2015-11-22 18:18:57 +0100

    Setting convention for command handlers

--- IoCCinema/IoCCinema/CompositionRoot/DIConfig.cs
+++ IoCCinema/IoCCinema/CompositionRoot/DIConfig.cs
@@ -2,12 +2,14 @@
 using Castle.Windsor;
 using IoCCinema.Business.Authentication;
 using IoCCinema.Business.Commands;
+using IoCCinema.Business.Lotery;
 using IoCCinema.Controllers;
 using IoCCinema.DataAccess;
 using IoCCinema.DataAccess.Business;
 using IoCCinema.DataAccess.Presentation;
 using IoCCinema.Presentation;
 using System.Web.Mvc;
+using System.Linq;
 
 namespace IoCCinema.CompositionRoot
 {
@@ -16,13 +18,20 @@ namespace IoCCinema.CompositionRoot
         internal static void Setup()
         {
             var container = new WindsorContainer();
+            var businessAssembly = typeof(ICommandHandler<>).Assembly;
             container.Register(Component.For<StringHasher>().LifeStyle.Singleton);
             container.Register(Component.For<ICurrentUserProvider>().ImplementedBy<ContextUserProvider>().LifeStyle.Singleton);
+            container.Register(Component.For<IWinChanceCalculatorFactory>().ImplementedBy<WindsorWinChanceCalculatorFactory>());
 
             container.Register(Classes.FromThisAssembly()
                 .BasedOn<Controller>()
                 .WithService.Self()
                 .Configure(c => c.LifestyleTransient()));
+
+            container.Register(Classes.FromAssembly(businessAssembly)
+                .BasedOn<IWinChanceCalculator>()
+                .WithService.Self()
+                .Configure(c => c.Named(c.Implementation.Name.Replace("UserWinChanceCalculator", string.Empty))));
 
             container.Register(Component.For<CinemaContext>().LifeStyle.PerWebRequest);
             container.Register(Classes.FromAssemblyContaining<CinemaContext>()
@@ -30,10 +39,22 @@ namespace IoCCinema.CompositionRoot
                 .WithService.FirstInterface()
                 .Configure(c => c.LifestylePerWebRequest()));
 
-            container.Register(Component.For<ICommandHandler<LoginCommand>>()
-                .ImplementedBy<TransactionalCommandHandler<LoginCommand>>().LifeStyle.PerWebRequest);
-            container.Register(Component.For<ICommandHandler<LoginCommand>>()
-                .ImplementedBy<LoginCommandHandler>().LifeStyle.PerWebRequest);
+            var commandTypes = businessAssembly.GetTypes()
+                .Where(t => !t.IsInterface && typeof(ICommand).IsAssignableFrom(t));
+
+            foreach(var commandType in commandTypes)
+            {
+                var handlerInterface = typeof(ICommandHandler<>).MakeGenericType(new[] { commandType });
+                var transactionalHandler = typeof(TransactionalCommandHandler<>).MakeGenericType(new[] { commandType });
+                container.Register(Component.For(handlerInterface)
+                    .ImplementedBy(transactionalHandler)
+                    .LifeStyle.PerWebRequest);
+            }
+
+            container.Register(Classes.FromAssembly(businessAssembly)
+                .BasedOn(typeof(ICommandHandler<>))
+                .WithService.FirstInterface()
+                .Configure(c => c.LifestyleTransient()));
 
             ControllerBuilder.Current.SetControllerFactory(new WindsorControllerFactory(container));
         }
---
+++ IoCCinema/IoCCinema/CompositionRoot/WindsorWinChanceCalculatorFactory.cs
@@ -0,0 +1,21 @@
+﻿using Castle.MicroKernel;
+using IoCCinema.Business;
+using IoCCinema.Business.Lotery;
+
+namespace IoCCinema.CompositionRoot
+{
+    public class WindsorWinChanceCalculatorFactory : IWinChanceCalculatorFactory
+    {
+        private readonly IKernel _kernel;
+
+        public WindsorWinChanceCalculatorFactory(IKernel kernel)
+        {
+            _kernel = kernel;
+        }
+
+        public IWinChanceCalculator GetCalculatorForUser(User user)
+        {
+            return (IWinChanceCalculator)_kernel.Resolve(user.UserType.ToString(), typeof(IWinChanceCalculator));
+        }
+    }
+}
