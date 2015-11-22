8d077fe3af8fff1f052816a82b012f42b968bec3
Mariusz Macheta <Mariusz.Macheta@gmail.com>
2015-11-22 15:53:36 +0100

    Convention for repositories and controllers

--- IoCCinema/IoCCinema/CompositionRoot/DIConfig.cs
+++ IoCCinema/IoCCinema/CompositionRoot/DIConfig.cs
@@ -16,17 +16,24 @@ namespace IoCCinema.CompositionRoot
         internal static void Setup()
         {
             var container = new WindsorContainer();
-            container.Register(Component.For<HomeController>().LifeStyle.Transient);
-            container.Register(Component.For<LoginController>().LifeStyle.Transient);
             container.Register(Component.For<StringHasher>().LifeStyle.Singleton);
             container.Register(Component.For<ICurrentUserProvider>().ImplementedBy<ContextUserProvider>().LifeStyle.Singleton);
 
+            container.Register(Classes.FromThisAssembly()
+                .BasedOn<Controller>()
+                .WithService.Self()
+                .Configure(c => c.LifestyleTransient()));
+
             container.Register(Component.For<CinemaContext>().LifeStyle.PerWebRequest);
-            container.Register(Component.For<ILoginViewRepository>().ImplementedBy<EfLoginRepository>().LifeStyle.PerWebRequest);
-            container.Register(Component.For<IMovieViewRepository>().ImplementedBy<EfMovieViewRepository>().LifeStyle.PerWebRequest);
-            container.Register(Component.For<IAuthenticationRepository>().ImplementedBy<EfAuthenticationRepository>().LifeStyle.PerWebRequest);
-            container.Register(Component.For<ICommandHandler<LoginCommand>>().ImplementedBy<TransactionalCommandHandler<LoginCommand>>().LifeStyle.PerWebRequest);
-            container.Register(Component.For<ICommandHandler<LoginCommand>>().ImplementedBy<LoginCommandHandler>().LifeStyle.PerWebRequest);
+            container.Register(Classes.FromAssemblyContaining<CinemaContext>()
+                .Where(t => t.Name.Contains("Repository"))
+                .WithService.FirstInterface()
+                .Configure(c => c.LifestylePerWebRequest()));
+
+            container.Register(Component.For<ICommandHandler<LoginCommand>>()
+                .ImplementedBy<TransactionalCommandHandler<LoginCommand>>().LifeStyle.PerWebRequest);
+            container.Register(Component.For<ICommandHandler<LoginCommand>>()
+                .ImplementedBy<LoginCommandHandler>().LifeStyle.PerWebRequest);
 
             ControllerBuilder.Current.SetControllerFactory(new WindsorControllerFactory(container));
         }
