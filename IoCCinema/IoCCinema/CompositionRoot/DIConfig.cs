f49dd6b5936f7da2eb39e38d21ab8836c196c411
Mariusz Macheta <Mariusz.Macheta@gmail.com>
2015-11-22 15:22:56 +0100

    Registering types for LoginController

--- IoCCinema/IoCCinema/CompositionRoot/DIConfig.cs
+++ IoCCinema/IoCCinema/CompositionRoot/DIConfig.cs
@@ -1,7 +1,10 @@
 ﻿using Castle.MicroKernel.Registration;
 using Castle.Windsor;
+using IoCCinema.Business.Authentication;
+using IoCCinema.Business.Commands;
 using IoCCinema.Controllers;
 using IoCCinema.DataAccess;
+using IoCCinema.DataAccess.Business;
 using IoCCinema.DataAccess.Presentation;
 using IoCCinema.Presentation;
 using System.Web.Mvc;
@@ -13,9 +16,17 @@ namespace IoCCinema.CompositionRoot
         internal static void Setup()
         {
             var container = new WindsorContainer();
-            container.Register(Component.For<CinemaContext>().LifeStyle.PerWebRequest);
             container.Register(Component.For<HomeController>().LifeStyle.Transient);
+            container.Register(Component.For<LoginController>().LifeStyle.Transient);
+            container.Register(Component.For<StringHasher>().LifeStyle.Singleton);
+            container.Register(Component.For<ICurrentUserProvider>().ImplementedBy<ContextUserProvider>().LifeStyle.Singleton);
+
+            container.Register(Component.For<CinemaContext>().LifeStyle.PerWebRequest);
+            container.Register(Component.For<ILoginViewRepository>().ImplementedBy<EfLoginRepository>().LifeStyle.PerWebRequest);
             container.Register(Component.For<IMovieViewRepository>().ImplementedBy<EfMovieViewRepository>().LifeStyle.PerWebRequest);
+            container.Register(Component.For<IAuthenticationRepository>().ImplementedBy<EfAuthenticationRepository>().LifeStyle.PerWebRequest);
+            container.Register(Component.For<ICommandHandler<LoginCommand>>().ImplementedBy<TransactionalCommandHandler<LoginCommand>>().LifeStyle.PerWebRequest);
+            container.Register(Component.For<ICommandHandler<LoginCommand>>().ImplementedBy<LoginCommandHandler>().LifeStyle.PerWebRequest);
 
             ControllerBuilder.Current.SetControllerFactory(new WindsorControllerFactory(container));
         }
