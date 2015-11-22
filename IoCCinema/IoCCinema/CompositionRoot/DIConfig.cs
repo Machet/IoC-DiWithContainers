3b6c7650852d416b184407c61610ea9257baadc0
Mariusz Macheta <Mariusz.Macheta@gmail.com>
2015-11-22 20:40:09 +0100

    Auditing

--- IoCCinema/IoCCinema/CompositionRoot/DIConfig.cs
+++ IoCCinema/IoCCinema/CompositionRoot/DIConfig.cs
@@ -10,6 +10,7 @@ using IoCCinema.DataAccess.Presentation;
 using IoCCinema.Presentation;
 using System.Web.Mvc;
 using System.Linq;
+using IoCCinema.DataAccess.AuditLogging;
 
 namespace IoCCinema.CompositionRoot
 {
@@ -19,9 +20,12 @@ namespace IoCCinema.CompositionRoot
         {
             var container = new WindsorContainer();
             var businessAssembly = typeof(ICommandHandler<>).Assembly;
+            var dataAccessAssembly = typeof(CinemaContext).Assembly;
+
             container.Register(Component.For<StringHasher>().LifeStyle.Singleton);
             container.Register(Component.For<ICurrentUserProvider>().ImplementedBy<ContextUserProvider>().LifeStyle.Singleton);
             container.Register(Component.For<IWinChanceCalculatorFactory>().ImplementedBy<WindsorWinChanceCalculatorFactory>());
+            container.Register(Component.For<AuditLogger>().LifeStyle.PerWebRequest);
 
             container.Register(Classes.FromThisAssembly()
                 .BasedOn<Controller>()
@@ -31,10 +35,11 @@ namespace IoCCinema.CompositionRoot
             container.Register(Classes.FromAssembly(businessAssembly)
                 .BasedOn<IWinChanceCalculator>()
                 .WithService.Self()
+                .LifestylePerWebRequest()
                 .Configure(c => c.Named(c.Implementation.Name.Replace("UserWinChanceCalculator", string.Empty))));
 
             container.Register(Component.For<CinemaContext>().LifeStyle.PerWebRequest);
-            container.Register(Classes.FromAssemblyContaining<CinemaContext>()
+            container.Register(Classes.FromAssembly(dataAccessAssembly)
                 .Where(t => t.Name.Contains("Repository"))
                 .WithService.FirstInterface()
                 .Configure(c => c.LifestylePerWebRequest()));
@@ -42,13 +47,28 @@ namespace IoCCinema.CompositionRoot
             var commandTypes = businessAssembly.GetTypes()
                 .Where(t => !t.IsInterface && typeof(ICommand).IsAssignableFrom(t));
 
-            foreach(var commandType in commandTypes)
+            var specificAuditingCommands = dataAccessAssembly.GetTypes()
+                .Where(t => !t.IsInterface && !t.IsGenericType && t.Name.Contains("Auditing"))
+                .ToList();
+
+            foreach (var commandType in commandTypes)
             {
                 var handlerInterface = typeof(ICommandHandler<>).MakeGenericType(new[] { commandType });
                 var transactionalHandler = typeof(TransactionalCommandHandler<>).MakeGenericType(new[] { commandType });
+                var auditingHandler = typeof(AuditingCommandHandler<>).MakeGenericType(new[] { commandType });
+
+                // transactional handlers
                 container.Register(Component.For(handlerInterface)
                     .ImplementedBy(transactionalHandler)
-                    .LifeStyle.PerWebRequest);
+                    .LifeStyle.Transient);
+
+                var specificHandler = specificAuditingCommands
+                    .FirstOrDefault(t => t.GetInterfaces().Any(i => i == handlerInterface));
+
+                var toRegister = specificHandler ?? auditingHandler;
+                container.Register(Component.For(handlerInterface)
+                    .ImplementedBy(toRegister)
+                    .LifeStyle.Transient);
             }
 
             container.Register(Classes.FromAssembly(businessAssembly)
