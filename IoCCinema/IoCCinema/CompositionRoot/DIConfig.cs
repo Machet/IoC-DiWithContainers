01cc9dff5eed2c662c523e6fb1f5fbd3a49e6f17
Mariusz Macheta <Mariusz.Macheta@gmail.com>
2015-11-24 20:50:03 +0100

    Registering components for login controller

--- IoCCinema/IoCCinema/CompositionRoot/DIConfig.cs
+++ IoCCinema/IoCCinema/CompositionRoot/DIConfig.cs
@@ -1,4 +1,7 @@
-﻿using IoCCinema.DataAccess;
+﻿using IoCCinema.Business.Authentication;
+using IoCCinema.Business.Commands;
+using IoCCinema.DataAccess;
+using IoCCinema.DataAccess.Business;
 using IoCCinema.DataAccess.Presentation;
 using IoCCinema.Presentation;
 using Microsoft.Practices.Unity;
@@ -10,8 +13,18 @@ namespace IoCCinema.CompositionRoot
         public static UnityContainer Setup()
         {
             var container = new UnityContainer();
+            container.RegisterType<ICurrentUserProvider, ContextUserProvider>(new ContainerControlledLifetimeManager());
             container.RegisterType<CinemaContext>(new PerRequestLifetimeManager());
             container.RegisterType<IMovieViewRepository, EfMovieViewRepository>(new PerRequestLifetimeManager());
+            container.RegisterType<ILoginViewRepository, EfLoginRepository>(new PerRequestLifetimeManager());
+            container.RegisterType<IAuthenticationRepository, EfAuthenticationRepository>(new PerRequestLifetimeManager());
+
+            container.RegisterType<ICommandHandler<LoginCommand>, LoginCommandHandler>("default");
+            container.RegisterType<ICommandHandler<LoginCommand>, TransactionalCommandHandler<LoginCommand>>(
+                new InjectionConstructor(
+                    new ResolvedParameter(typeof(ICommandHandler<LoginCommand>), "default"),
+                    new ResolvedParameter<CinemaContext>()));
+
             return container;
         }
     }
