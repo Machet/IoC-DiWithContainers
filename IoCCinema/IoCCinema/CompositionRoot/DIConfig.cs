0d0a717c5aad73ec52368fb41dc70ef59b4c6453
Mariusz Macheta <Mariusz.Macheta@gmail.com>
2015-11-24 01:13:15 +0100

    Unity added first registration made

---
+++ IoCCinema/IoCCinema/CompositionRoot/DIConfig.cs
@@ -0,0 +1,18 @@
+﻿using IoCCinema.Controllers;
+using IoCCinema.DataAccess.Presentation;
+using IoCCinema.Presentation;
+using Microsoft.Practices.Unity;
+using System.Web.Mvc;
+
+namespace IoCCinema.CompositionRoot
+{
+    public class DIConfig
+    {
+        public static void Setup()
+        {
+            var container = new UnityContainer();
+            container.RegisterType<IMovieViewRepository, EfMovieViewRepository>();
+            ControllerBuilder.Current.SetControllerFactory(new UnityControllerFactory(container));
+        }
+    }
+}
